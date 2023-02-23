06fbbe6ac1b7a8335eb480496a8eb20e10318854
Francisco Arriaga Velasco <arriaga.francisco@outlook.com>
2023-02-22 20:37:45 -0600

    Creamos interfaz con espacios para el mapa y charts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
+      <NavBar />
       <HomePage />
     </>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,41 @@
 import React from "react";
-import Chart from "../components/Chart";
-import Map from "../components/Map";
-import NavBar from "../components/NavBar";
+import "./pages.css";
+import TempChart from "../components/TempChart";
+import OtherChart from "../components/OtherChart";
+import Form from "../components/Form";
+import { Col, Container, Row } from "react-bootstrap";
+import MapComponent from "../components/MapComponent";
 
 const HomePage = () => {
   return (
-    <>
-      <NavBar />
-      <h1>Hola mundo</h1>
-      <Map />
-      <Chart />
-    </>
+    <Container>
+      <Row>
+        <h1 className="text-center">Mapa interactivo</h1>
+        <h2 className="text-center">
+          Muestra en tiempo real las condiciones meteorológicas en el mundo
+        </h2>
+        <p className="text-center">
+          Mover marcador rojo para seleccionar en automático las coordenadas a
+          analizar.
+        </p>
+      </Row>
+      <Row>
+        <Form />
+      </Row>
+      <Row className="mapAndData">
+        <Col className="mapContainer">
+          <MapComponent />
+        </Col>
+        <Col className="chartsContainer">
+          <Row className="chart1">
+            <TempChart />
+          </Row>
+          <Row className="chart2">
+            <OtherChart />
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
